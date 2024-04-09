8650cb423d8bb303b1d8f901453b03b60088aa75
JeongHanLim <40853979+lim-it-err@users.noreply.github.com>
2024-04-09 20:36:24 +0900

    Title: 코드트리 채점기, Time: 2162ms, Memory: 29MB, Status: Passed - CodeTree

--- 240409/코드트리 채점기/codetree-judger.py
+++ 240409/코드트리 채점기/codetree-judger.py
@@ -14,14 +14,10 @@ class Domain:
             return 0
         self.domain_id[uid] = t
         heapq.heappush(self.heap, (p, t, uid, self.name))
-        # if DEBUG:
-        #     print("After Insertion", self.heap, self.domain_id)
         return 1
     def delete(self, uid):
         _, _, _uid, _ = heapq.heappop(self.heap)
-        # print(uid, "deleted", self.name)
         del self.domain_id[uid]
-        # print(self.domain_id)
     def peek(self):
         if self.heap:
             return self.heap[0]
@@ -45,15 +41,20 @@ class WaitingQueue:
         self.data[domain_name].delete(uid)
         self.N -= 1
 
-    def get_candidate(self):
-        candidates = []
-        heapq.heapify(candidates)
+    def get_candidate(self, exclude):
+        gp, gt = 1e12, 1e12
+        ret_task = None
         for domain in self.data:
-            data = self.data[domain].peek()
-            if not data:
+            if domain in exclude:
                 continue
-            heapq.heappush(candidates, self.data[domain].peek())
-        return candidates
+            element = self.data[domain].peek()
+            if not element:
+                continue
+            p, t, x, y = element
+            if p<gp or (p == gp and t<gt):
+                ret_task = (p, t, x, y)
+                gp, gt = p, t
+        return ret_task
 
     def get_waiting_N(self):
         return self.N
@@ -70,7 +71,6 @@ class Judger():
         self.wq = wq
 
     def allocate_available(self, t, task):
-        # task : (p, t, uid, domain_name)
         p, _, uid, domain_name = task
         if not self.available:
             return False
@@ -81,28 +81,18 @@ class Judger():
         return True
 
     def start(self, t):
-        import time
-        s= time.perf_counter()
-        candidates = wq.get_candidate()
-        while candidates:
-            candidate = heapq.heappop(candidates)
-            _, _, uid, domain_name = candidate
-            if domain_name in self.working_domain:
-                continue  # Still Working
-            if domain_name in self.coldtime and t < self.coldtime[domain_name]:
-                continue  # in coldtime
-            if not self.allocate_available(t, candidate):
-                # if DEBUG:
-                #     print("300:No worker left")
-                return False
-            # if DEBUG:
-            #     print(f"300:{candidate} started working")
-            return True
+        exclude =list(self.working_domain.keys())+[domain_name for domain_name in self.coldtime if t<self.coldtime[domain_name]]
+        exclude = {k:0 for k in exclude}
+        candidates = wq.get_candidate(exclude)
+        if not candidates:
+            return False
+        if not self.allocate_available(t, candidates):
+            return False
+        return True
 
     def end(self, end_t, worker_id):
         if self.lst[worker_id] == ():
-            # if DEBUG:
-            #     print(f"400: {worker_id} was not in work. ignoring")
+
             return
         task = self.lst[worker_id]
         p, t, uid, domain_name = task
@@ -110,25 +100,48 @@ class Judger():
         del self.working_domain[domain_name]
         heapq.heappush(self.available, worker_id)
         self.lst[worker_id] = ()
-        # if DEBUG:
-        #     print(f"400: {task} are deleted, coldtime:{self.coldtime}")
         return
 
+# import sys
+# sys.stdin = open("input.txt")
 wq = WaitingQueue()
-N = int(input())
-jd = Judger(N, wq)
+Q = int(input())
+jd = None
 answer = []
-for i in range(N):
+import time
+s = time.perf_counter()
+for i in range(Q):
     cmd = input().split()
     if cmd[0] == "100":
         wq.add(0, 1, cmd[2])
+        jd = Judger(int(cmd[1]), wq)
+        # print(0, time.perf_counter() - s)
+        # s = time.perf_counter()
+        # 
+        # if DEBUG:
+        #     print(cmd, wq.get_waiting_N())
     if cmd[0] == "200":
         wq.add(int(cmd[1]), int(cmd[2]), cmd[3])
+        # print(1, time.perf_counter() - s)
+        # s = time.perf_counter()
+        # if DEBUG:
+        #     print(cmd, wq.get_waiting_N())
     if cmd[0] == "300":
         jd.start(int(cmd[1]))
+        # print(2, time.perf_counter() - s)
+        # s = time.perf_counter()
+        # if DEBUG:
+        #     print(cmd, wq.get_waiting_N())
     if cmd[0] == "400":
         jd.end(int(cmd[1]), int(cmd[2]) - 1)
+        # print(3, time.perf_counter() - s)
+        # s = time.perf_counter()
+        # if DEBUG:
+        #     print(cmd, wq.get_waiting_N())
     if cmd[0] == "500":
         answer.append(wq.get_waiting_N())
+        # print(4, time.perf_counter() - s)
+        # s = time.perf_counter()
+# print("RET", time.perf_counter()-s)
 for i in answer:
     print(i)
